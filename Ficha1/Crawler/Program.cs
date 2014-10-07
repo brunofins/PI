348954140c60a97bc95cdaa29f48bce9645b2be0
DanielAbreu <dani_abreu87@hotmail.com>
2014-10-07 20:13:44 +0100

    Crawler updated

--- Ficha1/Crawler/Program.cs
+++ Ficha1/Crawler/Program.cs
@@ -9,6 +9,9 @@ namespace Crawler
 {
     class Program
     {
+        private static Dictionary<String,List<String>> wordsAndURLs= new Dictionary<String,List<String>>();
+
+
         static void Main(string[] args)
         {
             if (args.Length != 2)
@@ -18,41 +21,61 @@ namespace Crawler
 
             IndexWords(args[0], Convert.ToInt32(args[1]));
 
-            ReadWord();
+            
 
-            GetUrls();
+            GetUrls(ReadWord());
 
 
         }
 
-        private static void GetUrls()
+        private static void GetUrls(string word)
         {
-            throw new NotImplementedException();
+            System.Console.WriteLine("Search results:");
+            foreach (String url in wordsAndURLs[word])
+                System.Console.WriteLine(url);
         }
 
-        private static void ReadWord()
+        private static String ReadWord()
         {
             System.Console.WriteLine("Write a word to search:");
             System.Console.Write(">> ");
-            String wordToSearch = System.Console.ReadLine();
-            System.Console.WriteLine(wordToSearch);
+            return System.Console.ReadLine(); 
         }
 
         private static void IndexWords(String url, int depth)
         {
+            if (depth == 0)
+                return;
+
             HtmlWeb web = new HtmlWeb();
             HtmlAgilityPack.HtmlDocument doc = web.Load(url);
 
-            HtmlAgilityPack.HtmlNode bodyNode = doc.DocumentNode.SelectSingleNode("//body");
+            //HtmlAgilityPack.HtmlNode bodyNode = doc.DocumentNode.SelectSingleNode("//a");
 
-            String test1 = bodyNode.InnerHtml;//Obtemos o codigo html do body
-            System.Console.WriteLine(test1);
-            System.Console.WriteLine("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-            String test2 = bodyNode.InnerText;//Obtemos o texto do body sem as tags
-            System.Console.WriteLine(test2);
+            String nodeString = doc.DocumentNode.OuterHtml;
+            //foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//a"))//tentativa de ir buscar todo o texto da pagina html mas ainda nao sei como
+                //nodeString += node.InnerText;
+
+            IEnumerable<HtmlNode> urls = doc.DocumentNode.Descendants().Where(x => x.Attributes.Contains("href"));
+
+            System.Console.WriteLine(nodeString);
+            String[] wordsArray = nodeString.Split(' ');
+
+            foreach (String s in wordsArray)
+                if (wordsAndURLs.ContainsKey(s)) { 
+                    if (!wordsAndURLs[s].Contains(url))
+                        wordsAndURLs[s].Add(url);
+                }
+                else
+                {
+                    List<String> urlList = new List<String>();
+                    urlList.Add(url);
+                    wordsAndURLs.Add(s, urlList);
+                }
+
+            /*foreach (var link in urls)
+                IndexWords(link.Attributes["href"].Value, --depth);*/
             
-            int stop = 0;// so para parar o programa aqui para fazer debug
-          
         }
     }
 }
